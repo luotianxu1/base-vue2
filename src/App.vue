af859e89da87d51762125b87c50bee7c63a404c5
罗天旭 <1265062404@qq.com>
2024-09-13 08:36:50 +0800

    feat(custom): 代码格式化

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,16 @@ export default {
 		HelloWorld
 	}
 }
+
 </script>
 
 <style>
 #app {
+	margin-top: 60px;
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
 	color: #2c3e50;
-	margin-top: 60px;
+	text-align: center;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -42,7 +42,7 @@
 export default {
 	name: "HelloWorld",
 	props: {
-		msg: String
+		msg: string
 	}
 }
 </script>
@@ -52,14 +52,17 @@ export default {
 h3 {
 	margin: 40px 0 0;
 }
+
 ul {
-	list-style-type: none;
 	padding: 0;
+	list-style-type: none;
 }
+
 li {
 	display: inline-block;
 	margin: 0 10px;
 }
+
 a {
 	color: #42b983;
 }
