fd3fc0b0f68c9469aa99c675c55e07f76f9b500b
罗天旭 <1265062404@qq.com>
2024-09-13 09:03:51 +0800

    feat(custom): 第三方组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
 	<div id="app">
-		<img alt="Vue logo" src="./assets/logo.png" />
 		<HelloWorld msg="Welcome to Your Vue.js App" />
 	</div>
 </template>
@@ -14,16 +13,6 @@ export default {
 		HelloWorld
 	}
 }
-
 </script>
 
-<style>
-#app {
-	margin-top: 60px;
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	color: #2c3e50;
-	text-align: center;
-}
-</style>
+<style></style>
